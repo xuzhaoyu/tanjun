e06210202fc20f1e65eae9d8c655b0aafa8149b8
xuzhaoyu <xuzhaoyu@gmail.com>
2014-12-01 16:09:30 +0800

    a

--- app/views/data/setThreshold.blade.php
+++ app/views/data/setThreshold.blade.php
@@ -1,4 +1,24 @@
 {{ Form::open(array('route' => 'range')) }}
-{{ Form::label('dust', 'Dust'); }}
-{{ Form::text('name', 'value'); }}
+{{ Form::label('tempMax', 'tempMax'); }}
+{{ Form::text('tempMax', 'value'); }}
+{{ 'C' }}
+{{ Form::label('tempMin', 'tempMin'); }}
+{{ Form::text('tempMin', 'value'); }}
+{{ Form::label('humidityMax', 'humidityMax'); }}
+{{ Form::text('humidityMax', 'value'); }}
+{{ Form::label('humidityMin', 'humidityMin'); }}
+{{ Form::text('humidityMin', 'value'); }}
+{{ Form::label('pressureMax', 'pressureMax'); }}
+{{ Form::text('pressureMax', 'value'); }}
+{{ Form::label('pressureMin', 'pressureMin'); }}
+{{ Form::text('pressureMin', 'value'); }}
+{{ Form::label('smokeMax', 'smokeMax'); }}
+{{ Form::text('smokeMax', 'value'); }}
+{{ Form::label('smokeMin', 'smokeMin'); }}
+{{ Form::text('smokeMin', 'value'); }}
+{{ Form::label('dustMax', 'DustMax'); }}
+{{ Form::text('dustMax', 'value'); }}
+{{ Form::label('dustMin', 'DustMin'); }}
+{{ Form::text('dustMin', 'value'); }}
+{{ Form::submit('Submit') }}
 {{ Form::close() }}
