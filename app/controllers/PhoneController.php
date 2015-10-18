603c92b4b00815624797cd84a4bb3ba134028390
Zhaoyu Xu <xuzhaoyu@gmail.com>
2015-10-18 16:02:17 +0800

    numbers

--- app/controllers/PhoneController.php
+++ app/controllers/PhoneController.php
@@ -9,10 +9,18 @@ class PhoneController extends \BaseController
 {
     public function postData(){
         $input = Input::all();
+        $all_numbers = DB::table('numbers')->select('phone')->get();
+        $phone = $input['phone'];
+        foreach ($all_numbers as $number){
+            if($number == $input['phone']){
+                $phone = $input['phone']."abc";
+                break;
+            }
+        }
         DB::table('received')->insert(array(
             'client_time' => $input['client'],
             'IMEI' => $input['IMEI'],
-            'phone' => $input['phone']."abc"
+            'phone' => $phone
         ));
         return 'Success';
     }
